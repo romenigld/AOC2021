4442360a60c600a65e8d2135a8f068b0a9ea714d
Romenig Lima Damasio <romenigld@gmail.com>
2021-12-03 18:49:59 +0100

    changed the value @sample for use the multiline string

--- lib/day02.ex
+++ lib/day02.ex
@@ -1,12 +1,13 @@
 defmodule Day02 do
-  @sample [
-    "forward 5",
-    "down 5",
-    "forward 8",
-    "up 3",
-    "down 8",
-    "forward 2"
-  ]
+  @sample """
+forward 5
+down 5
+forward 8
+up 3
+down 8
+forward 2
+"""
+  |> String.split(~r/\n/, trim: true)
 
   @day02_file "assets/day02.txt"
 
